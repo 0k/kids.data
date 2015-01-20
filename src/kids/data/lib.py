5d6c2f2258f13f76f80b86e1e2220eebcf24ad4c
Valentin Lab <valentin.lab@kalysto.org>
2015-01-20 16:00:13 +0700

    new: python3 support and added tests for better coverage.

--- src/kids/data/lib.py
+++ src/kids/data/lib.py
@@ -11,8 +11,23 @@ def partition(elts, f):
     return res
 
 
-def first(elts, f):
+def first(elts, predicate):
+    """Returns the first elt of elts that matches predicate
+
+        >>> first([3, 7, 11, 15, 33], predicate=lambda x: x % 11 == 0)
+        11
+
+    But if no element matches the predicate it should cast an
+    exception::
+
+        >>> first([3, 7, 11, 15, 33], predicate=lambda x: x % 13 == 0)
+        Traceback (most recent call last):
+        ...
+        ValueError: No value matches predicate
+
+
+    """
     for elt in elts:
-        if f(elt):
+        if predicate(elt):
             return elt
     raise ValueError("No value matches predicate")
